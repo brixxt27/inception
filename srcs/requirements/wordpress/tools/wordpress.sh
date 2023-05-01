1609aba5aae851a608529f3987db199af3b6a9d5
brixxt27 <brixxt27@gmail.com>
2023-05-01 22:09:25 +0900

    fix: wordpress 가 mysql 과 연결되지 못함 #17 #11
    mysql 을 연결 못하길래 wp-config.php 파일과 관련 있는줄 알았는데 wp
    config create --path=/var/www/html --dbname=wordpress --dbuser=jayoon
    --dbpass=1234 --dbhost=mysql:3306 이 명령어 자체가 wp-config.php 파일을
    만드는 명령어이고, mysql 이 해당 이름과 비밀번호를 환경변수로 켜지지
    않은 것 같다.

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -3,6 +3,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download --path=/var/www/html --locale=ko_KR
+#mv /wp-config.php /var/www/html/wp-config.php
 wp config create --path=/var/www/html --dbname=wordpress --dbuser=jayoon --dbpass=1234 --dbhost=mysql:3306
 php-fpm8 -F
-
