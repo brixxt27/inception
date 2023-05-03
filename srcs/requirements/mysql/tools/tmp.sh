7ffb0621352000ed461a50e5795612f0aa0ccfc8
brixxt27 <brixxt27@gmail.com>
2023-05-03 20:43:04 +0900

    feat: 환경변수 이용 및 mysql 접속 완료 #17 #18
    - 환경변수 이용하여 root 비밀번호, 이용자 및 데이터베이스 생성 완료.
      이는 mysqld 를 실행해야 했고, bootstrap 을 해야 한다.
    - 환경변수 적용 후 /usr/bin/mysqld --user=mysql & 를 하면 계속 재시작
      했는데 이는 데몬으로 실행되기 때문에 docker exec -it 해서 접근하지 않을 때는 bash 실행되지 않아 pid 1 이 없어 restart: always 로 계속 재시작 된다.
    - make 를 통해 mysql 컨테이너에서 bash 를 실행시키지 않았다면 무조건
      포그라운드로 실행해줘야 한다. 결론적으로 bash 로 실행하여 mysql -u root -p 를 통해
      환경변수를 이용하여 만들어진 것을 확인했다.

--- srcs/requirements/mysql/tools/tmp.sh
+++ srcs/requirements/mysql/tools/tmp.sh
@@ -36,3 +36,95 @@ EOF
 echo this is endpoint!
 
 exec /usr/bin/mysqld --user=mysql --console	
+
+
+
+
+
+
+--------------
+mysql -u root -p"$MYSQL_ROOT_PASSWORD"<<-EOSQL
+        CREATE DATABASE $MYSQL_DATABASE;
+EOSQL
+
+
+-----------------
+mkdir -p /auth_pam_tool_dir/auth_pam_tool
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+
+/usr/bin/mysqld --user=mysql --console	
+openrc
+touch /run/openrc/softlevel
+/etc/init.d/mariadb setup
+/etc/init.d/mariadb start
+------------------
+#!/bin/sh
+touch /run/openrc/softlevel
+openrc
+
+touch /auth_pam_tool_dir
+
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+
+mysql -u root << EOSQL
+	alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';
+	flush privileges;
+
+	create database $MYSQL_DATABASE;
+	create user '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';
+	grant all privileges on $MYSQL_DATABASE.* to '$MYSQL_USER'@'%';
+
+	flush privileges;
+EOSQL
+
+echo "Hello MySQL!"
+/usr/bin/mysqld --user=mysql --console	
+
+
+
+
+
+
+
+
+------------------
+#!/bin/sh
+touch /run/openrc/softlevel
+openrc
+
+#mkdir -p /auth_pam_tool_dir/auth_pam_tool
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+#mysql_install_db --user=mysql --ldata=/var/lib/mysql
+
+/usr/bin/mysqld --user=mysql --bootstrap << ESQL
+use mysql;
+flush privileges;
+
+alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';
+flush privileges;
+
+create database $MYSQL_DATABASE;
+create user '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';
+grant all privileges on $MYSQL_DATABASE.* to '$MYSQL_USER'@'%';
+
+flush privileges;
+ESQL
+
+# demon 이 켜지지 않은 상태에서 mysql 을 실행하면 soket 이 생성되지 않는다.
+# 그러면 mysql client 를 실행할 수 없다면서 에러가 발생한다.
+#mysql -u root << EOSQL
+#	alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';
+#	flush privileges;
+
+#	create database $MYSQL_DATABASE;
+#	create user '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';
+#	grant all privileges on $MYSQL_DATABASE.* to '$MYSQL_USER'@'%';
+
+#	flush privileges;
+#EOSQL
+
+echo "Hello MySQL!"
+#/usr/bin/mysqld --user=mysql --console	
+# mysql(-server) 를 데몬으로 실행함. 평소 내가 알고 있는 mysql 과 mariadb 를 실행하는 것은 client 로 실행하는 것이다.
+# 아래를 실행해야 soket 이 생성되면서 mysql(mariadb) 를 실행할 수 있다.
+/usr/bin/mysqld --user=mysql &
